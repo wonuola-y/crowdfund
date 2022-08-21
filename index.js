886aa3d12a0a5900ba7c5324335516c3f91ce0e6
Wonuola <wonuolalonge@gmail.com>
2022-08-21 14:38:29 +0100

    creating modal page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const btn = document.querySelectorAll('.reward-btn')
-const modal = document.querySelector('.modal')
+const modal = document.querySelector('.modal-container')
 const mainContent = document.querySelector('.third')
 const cancel = document.querySelector('.cancel')
 const main = document.querySelector('main')
@@ -8,14 +8,15 @@ console.log(modal)
 console.log(btn);
 for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener('click', ()=>{
+        modal.style.display = 'flex';
   modal.classList.toggle('modal-show')
   window.scrollTo(150, 200)
   main.style.backgroundColor = 'light gray'
-  mainContent.classList.toggle('behind')
+//   mainContent.classList.toggle('behind')
 cancel.addEventListener('click', ()=>{
     modal.style.display = 'none';
     mainContent.style.backgroundColor = 'white';
-    mainContent.classList.remove('behind')
+    // mainContent.classList.remove('behind')
 })
     })
     
